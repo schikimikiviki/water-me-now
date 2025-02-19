cd57ad38f8b5cf63f20fe9723fbb45c41d3aa153
schikimikiviki <viktoria.bzdyk@gmx.at>
2025-02-19 16:21:47 +0100

    basic classes and enum in backend

---
+++ backend/src/main/java/com/plants/data/Common_pest.java
@@ -0,0 +1,84 @@
+package com.plants.data;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "common_pest")
+public class Common_pest {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String name; 
+	private String imageFile; 
+	private String todo; 
+	@ManyToMany
+	@JoinTable(
+	    name = "plant_pest",
+	    joinColumns = @JoinColumn(name = "pest_id"),
+	    inverseJoinColumns = @JoinColumn(name = "plant_id")
+	)
+	private List<Plant> plantList;
+	
+	public Common_pest() {}
+
+	
+	public Common_pest (Long id, String name, String imageFile, String todo, List<Plant> plantList) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.imageFile = imageFile;
+		this.todo = todo;
+		this.plantList = plantList;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImageFile() {
+		return imageFile;
+	}
+
+	public void setImageFile(String imageFile) {
+		this.imageFile = imageFile;
+	}
+
+	public String getTodo() {
+		return todo;
+	}
+
+	public void setTodo(String todo) {
+		this.todo = todo;
+	}
+
+	public List<Plant> getPlantList() {
+		return plantList;
+	}
+
+	public void setPlantList(List<Plant> plantList) {
+		this.plantList = plantList;
+	}
+}
---
+++ backend/src/main/java/com/plants/data/Soil_type.java
@@ -0,0 +1,14 @@
+package com.plants.data;
+
+public enum Soil_type {
+	CULTIVATION_SOIL, // Anzuchterde
+	PALM_SOIL, // Palmenerde
+	POTTING_SOIL, // Blumenerde
+	CACTUS_SOIL, // Kakteenerde
+	RHODODENDRON_SOIL, // Rhododendronerde
+	CITRUS_SOIL, // Zitruserde
+	ORCHID_SOIL, // Orchideenerde
+	TOMATO_SOIL, // Tomatenerde
+	COMPOST // Kompost
+	
+}
