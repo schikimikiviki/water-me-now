a9601bec35343c9e5c449d8ce1be0dda9279f332
schikimikiviki <viktoria.bzdyk@gmx.at>
2025-02-23 14:14:45 +0100

    GET requests from frontend

--- frontend/src/components/PestList/PestList.jsx
+++ frontend/src/components/PestList/PestList.jsx
@@ -1,9 +1,31 @@
 import './PestList.css';
+import { useState, useEffect } from 'react';
+import getRequest from '../../helpers/functions';
 
 function PestList() {
+  const [pests, setPests] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let pests = await getRequest('pests');
+      setPests(pests);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className='page-div'>
       <h1>Pests</h1>
+      {pests && pests.length > 1 ? (
+        <ul>
+          {pests.map((pest) => (
+            <li key={pest.id}>{pest.name}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>Loading pests...</p>
+      )}
     </div>
   );
 }
--- frontend/src/components/TaskList/TaskList.jsx
+++ frontend/src/components/TaskList/TaskList.jsx
@@ -1,7 +1,30 @@
+import { useState, useEffect } from 'react';
+import getRequest from '../../helpers/functions';
+
 function TaskList() {
+  const [tasks, setTasks] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let tasks = await getRequest('plant-tasks');
+      setTasks(tasks);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className='page-div'>
       <h1>Tasks</h1>
+      {tasks && tasks.length > 1 ? (
+        <ul>
+          {tasks.map((task) => (
+            <li key={task.id}>{task.name}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>Loading tasks...</p>
+      )}
     </div>
   );
 }
