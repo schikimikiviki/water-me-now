c6842557091f71f26bd6688d4cb365bfab2f4819
schikimikiviki <viktoria.bzdyk@gmx.at>
2025-02-23 18:09:26 +0100

    basic styles for frontend

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,7 +1,22 @@
 function Header() {
   return (
-    <div style={{ padding: '30px', top: '0px' }}>
+    <div
+      style={{
+        padding: '30px',
+        top: '0px',
+        border: '2px solid white',
+        borderRadius: '20px',
+        margin: '30px',
+        backgroundColor: '#95b8d1',
+        width: '90vw',
+        marginTop: '-150px',
+      }}
+    >
       <h1>🪴 water me now 🪴</h1>
+      <hr style={{ width: '500px', marginLeft: '0' }}></hr>
+      <p style={{ fontSize: '22px', marginLeft: '100px' }}>
+        Plant management app
+      </p>
     </div>
   );
 }
