b31b747d0de0a661dbae48d2a0c2ea4daa320ea9
schikimikiviki <viktoria.bzdyk@gmx.at>
2025-03-03 16:05:14 +0100

    better ui for plantList

--- backend/src/main/java/com/plants/backend/security/WebConfig.java
+++ backend/src/main/java/com/plants/backend/security/WebConfig.java
@@ -2,11 +2,18 @@ package com.plants.backend.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+	
+	  @Override
+	    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	        registry.addResourceHandler("/uploads/**")
+	                .addResourceLocations("file:uploads/");
+	    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
